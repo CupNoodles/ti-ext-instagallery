171e1e7dd7924b9dfecd7d574a4f7e3dd04dd380
CupNoodles <daigo.w.kawasaki@gmail.com>
2021-10-18 11:22:58 -0400

    probably fix for the cron update issue

--- controllers/InstaAccounts.php
+++ controllers/InstaAccounts.php
@@ -103,6 +103,7 @@ class InstaAccounts extends \Admin\Classes\AdminController
 
         if(!is_object($account_id)){
             $model = $this->formFindModelObject($account_id);
+            $account_id = $model->id;
         }
         else{
             $model = $account_id;
